4915a23bb2e487837194ac449ede5faf78231f8a
DingyinSun <126644895+DingyinSun@users.noreply.github.com>
2023-07-31 16:51:38 +1000

    Update characterise_mutation_type.sh

--- characterise_mutation_type.sh
+++ characterise_mutation_type.sh
@@ -6,9 +6,9 @@ Gaps=$(less ${file} | grep "Gaps" | sed 's/.*(\([^)]*\)).*/\1/' | tr -d '%')
 name1=${file#blastp_out/}
 name2=${name1%.out}
  if [ $(echo "${Identity} == 100.0 && ${Gaps} == 0.0" | bc -l) -eq 1 ];then
-  echo -e "${name2}\tNonsynonymous_mutation" >> characterise_mutation_type.txt
- elif [ $(echo "${Gaps} == 0.0 && ${Identity} != 100.0" | bc -l) -eq 1 ];then
   echo -e "${name2}\tSynonymous_mutation" >> characterise_mutation_type.txt
+ elif [ $(echo "${Gaps} == 0.0 && ${Identity} != 100.0" | bc -l) -eq 1 ];then
+  echo -e "${name2}\tNonsyonymous_mutation" >> characterise_mutation_type.txt
  else
   echo -e "${name2}\tother_mutation" >> characterise_mutation_type.txt
  fi
