1bd97944807c96bbfe57a8fb20cc0dc01c01bc25
DingyinSun <126644895+DingyinSun@users.noreply.github.com>
2023-08-01 16:36:47 +1000

    Update blastp.sh

--- blastp.sh
+++ blastp.sh
@@ -16,3 +16,5 @@ name2=${name1%_1.pep}
 name3=${file%_1.pep}
 needle -asequence ${name3}_3.pep -bsequence ${file} -gapopen 10.0 -gapextend 0.5 -outfile blastp_out/${name2}_blastp_3.out;
 done
+
+## Add more for-loop if there is more than 3 Clusters
